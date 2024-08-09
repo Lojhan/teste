bbb1422cf9c47205c2b4090b9d362806d346e487
lojhan <viniciuslojhan@gmail.com>
2024-08-09 14:55:06 -0300

    teste

--- teste.js
+++ teste.js
@@ -1,1 +1,19 @@
-console.log("test")
+function teste() {
+  const a = 1;
+  const b = 2;
+
+  const x = a + b;
+
+  console.log(x);
+  console.log("teste");
+}
+
+
+setTimeout(() => {
+    teste();
+}, 1000);
+
+setInterval(() => {
+    teste();
+    teste();
+}, 1000);
